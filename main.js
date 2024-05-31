e0932275a8ccd614603f775ec316101187e5abfb
OM SANTOSH GAWANDE <gawandeom98@gmail.com>
2024-05-31 19:20:00 +0530

    Create Some new Files

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ const goHome = () => {
 const createStudent = () => {
     const mainContent = document.querySelector('.main-content');
     console.log(mainContent);
-    if(!document.querySelector('.form-container')){
+    mainContent.innerHTML="";
+    mainContent.innerText="";
         const formContainer = document.createElement('div');
     formContainer.classList.add('form-container');
     formContainer.classList.add('flex');
@@ -119,12 +120,66 @@ const createStudent = () => {
 
     formContainer.appendChild(studentForm);
     mainContent.appendChild(formContainer);
-    }
-    
 }
 
-const showStudent = () => {
-    alert("Student Data is Showing here");
+const showStudent = async() => {
+    // await fetch("Student.json").then(response => response.json()).then(data => console.log(data)).catch((error) => console.log(error));
+
+    const mainContainer = document.querySelector('.main-content');
+    mainContainer.innerHTML="";
+    const tableContainer = document.createElement("div");
+    tableContainer.classList.add('table-container');
+
+    const response = await fetch("Student.json");
+    const data = await response.json();
+    const students = Array.from(data);
+
+    const table = document.createElement('table');
+
+
+    const row = document.createElement('tr');
+    const nameHead = document.createElement("th");
+    nameHead.innerText= "STUDENT NAME";
+
+
+    const classHead = document.createElement("th");
+    classHead.innerText= "CLASS";
+
+    const dobHead = document.createElement("th");
+    dobHead.innerText= "BIRTH DATE";
+
+    const genderHead = document.createElement("th");
+    genderHead.innerText= "GENDER";
+
+    row.appendChild(nameHead);
+    row.appendChild(classHead);
+    row.appendChild(dobHead);
+    row.appendChild(genderHead);
+
+    table.appendChild(row);
+
+    students.forEach(student => {
+        const dataRow = document.createElement("tr");
+        const nameData = document.createElement('td');
+        const classData = document.createElement('td');
+        const dobData = document.createElement('td');
+        const genderData = document.createElement('td');
+        nameData.innerText= `${student.name}`;
+        classData.innerText=  `${student.className}`;
+        dobData.innerText=  `${student.dob}`;
+        genderData.innerText=  `${student.gender}`;
+
+        dataRow.appendChild(nameData);
+        dataRow.appendChild(classData);
+        dataRow.appendChild(dobData);
+        dataRow.appendChild(genderData);
+
+        table.appendChild(dataRow);
+    });
+
+    tableContainer.appendChild(table);
+    mainContainer.appendChild(tableContainer);
+    
 }
 
 const editStudent = () => {
